e9a1f9d780a58ab30401896abe364a0776562a00
Keimah507 <74242600+Keimah507@users.noreply.github.com>
2022-06-01 00:19:03 +0300

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-def fizzbuzz();
+def fizzbuzz():
+    
+    
 for i int range(1, 101):
-    if i % 5 == 0 and i % 3 ==0:
+    if i % 5 == 0 and i % 3 == 0:
         print("{}".format("FizzBuzz"), end="")
     elif i % 3 == 0:
         print("{}".format("Fizz"), end="")
