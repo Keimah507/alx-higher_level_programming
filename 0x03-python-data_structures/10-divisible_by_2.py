82a7625d9408ac0a18e0451b6460db1e4b7852b8
root <githinjibrian5@gmail.com>
2022-06-06 03:57:09 -0700

    task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    checK_div = []
+
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            check_div.appent(True)
+        else:
+            chech_div.append(False)
+
+    return (chech_div)
