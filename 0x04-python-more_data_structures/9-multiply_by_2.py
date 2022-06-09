798e830f0d2b031981713eb477ce011c3c7135b1
Keimah507 <74242600+Keimah507@users.noreply.github.com>
2022-06-09 03:00:58 +0300

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     b_dictionary = {}
-    for k, v in a_dictionary.items():
-        b_dictionary[k] = v * 2
-    return b_dictionary
+    if a_dictionary:
+        for key, val in a_dictionary.items():
+            b_dictonary.update({key: val * 2)}
+    return (b_dictionary)
