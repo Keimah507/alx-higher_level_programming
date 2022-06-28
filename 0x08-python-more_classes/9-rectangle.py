896fa64960eb3f677257ca9a651b2fe11a3e4d16
root <githinjibrian5@gmail.com>
2022-06-28 13:02:23 -0700

    Hot fix

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -6,7 +6,7 @@ Defines a class Rectangle
 
 class Rectangle:
     """Representation of a rectangle"""
-    
+
     number_of_instances = 0
     print_symbol = "#"
 
@@ -18,13 +18,13 @@ class Rectangle:
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """returns the biggest rectangle based on the area"""
-    if type(rect_1) is not Rectangle:
-        raise TypeError("rect_1 must be an instance of Rectangle")
-    if type(rect_2) is not Rectangle:
-        raise TypeError("rect_2 must be an instance of Rectangle")
-    if rect_1.area() >= rect_2.area():
-        return rect_1
-    return rect_2
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
 
     def __init__(self, width=0, height=0):
         """Initializes the rectangle"""
@@ -40,7 +40,7 @@ class Rectangle:
     @property
     def width(self):
         """getter for the private instance attribute width"""
-    return self.__width
+        return self.__width
 
     @width.setter
     def width(self, value):
