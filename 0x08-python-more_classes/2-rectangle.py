ccb453f7f58985973a02f8a4334a84ed6a9856e9
root <githinjibrian5@gmail.com>
2022-06-28 12:09:31 -0700

    0x08

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -14,6 +14,11 @@ class Rectangle:
     @property
     def width(self):
         """getter for the private instance attribute width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter for the private instance attribute width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
