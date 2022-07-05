f96096e2f72cd3cfe71cc8ee680141723671385c
root <githinjibrian5@gmail.com>
2022-07-05 16:24:46 -0700

    0x0B

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Contains a function that reads & prints a files contents
+"""
+
+
+def read_file(filename=""):
+    """Function that reads a file and prints the contents
+    read.
+    Args:
+        filename (str): the string that contains the filename
+    """
+    with open(filename, encoding="utf8") as f:
+        print(f.read(), end="")
