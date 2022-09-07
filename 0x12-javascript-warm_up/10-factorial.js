d915930b96a89ce2c11c8521016d43c57cef90de
Keimah507 <githinjibrian5@gmail.com>
2022-09-07 15:43:32 +0000

    Fixed if statements

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,11 +2,11 @@
 function factorialize (num) {
   if (num < 0) {
     return (-1);
-  } else if (num === undefined) {
-    return (1);
-  } else {
-    return (num * factorialize(num - 1));
   }
+  if (num === 0 || isNaN(num)) {
+    return (1);
+  } 
+  return (num * factorialize(num - 1));
 }
 
 console.log(factorialize(Number(process.argv[2])));
