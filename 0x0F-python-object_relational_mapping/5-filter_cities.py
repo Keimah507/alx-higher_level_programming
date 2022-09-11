1b19dd9ea5b5f0837cd8e0516f7d570e6eb48ba3
root <githinjibrian5@gmail.com>
2022-09-11 12:09:41 -0700

    Task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -2,13 +2,12 @@
 """Lists all cities from database hbtn_0e_4_usa"""
 
 
-if __name__ = '__main__':
-    
+if __name__ == '__main__':
     import MySQLdb
     import sys
 
     db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.arg[3])
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT cities.name\
