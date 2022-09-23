6f9f84e4165c6557e66c0079cb37631f9a0737bb
root <githinjibrian5@gmail.com>
2022-09-23 09:28:12 -0700

    0x11

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """fetches the URL"""
 
-if__name__ == "__main__":
+if __name__ == "__main__" :
     import requests
 
     r = requests.get('https://intranet.hbtn.io/status')
 
     print('Body response:')
     print('\t- content: {}'.format(type(r.text)))
-    print('\t- content: {}'.fomat(r.text))
+    print('\t- content: {}'.format(r.text))
