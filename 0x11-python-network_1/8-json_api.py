0d9dc6747ce3280c30a9d54283a8afef5fcefe3d
root <githinjibrian5@gmail.com>
2022-09-23 09:16:41 -0700

    add 8-json_apipy

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,17 +2,17 @@
 """takes in a letter and sends POST request to URL with letter as parameter"""
 
 
-if __name__ == "__main__"
-import requests
-import sys
+if __name__ == "__main__":
+    import requests
+    import sys
 
-q = sys.argv[1] if len(sys.argv) > 1 else ""
-r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
-try:
-    dict = r.json()
-    if dic == {}:
-        print('No result')
-    else:
-        print("[{}] {}".format(dict.get('id'), dict.get('name')))
-except ValueError:
-    print('Not a valid JSON')
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        dic = r.json()
+        if dic == {}:
+            print('No result')
+        else:
+            print("[{}] {}".format(dic.get('id'), dic.get('name')))
+    except ValueError:
+        print('Not a valid JSON')
