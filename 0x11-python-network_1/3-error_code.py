f0dece08c4854da27083965af95b2bf2eb1a95a2
root <githinjibrian5@gmail.com>
2022-09-23 09:31:58 -0700

    0x11

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Takes in URL, sends request to passed URL and displays body of response
 passed in utf-8"""
+
+
 if __name__ == "__main__":
     import urllib.request
     from urllib.error import HTTPError
@@ -9,5 +11,5 @@ if __name__ == "__main__":
     try:
         with urllib.request.urlopen(sys.argv[1]) as response:
             print(response.read().decode("utf-8", "replace"))
-    except HTTPError as T:
+    except HTTPError as e:
         print("Error code: {}".format(e.code))
