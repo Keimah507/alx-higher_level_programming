09deeb4a953f18450190c01213e7cead9e9e1328
root <githinjibrian5@gmail.com>
2022-09-23 09:36:12 -0700

    0x11

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,7 +8,7 @@ from sys import argv
 
 if __name__ == "__main__":
     req = requests.get(argv[1])
-    if res.status_code >= 400:
-        print("Error code: {}".format(res.status_code))
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
     else:
         print(res.text)
